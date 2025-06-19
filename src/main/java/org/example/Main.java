bafd0bfa2d9a52952dd226f1388b9a5966100b62
User <vikaorlyanina97@gmail.com>
2025-06-19 12:56:50 +0300

    fix main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,175 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //1
+        printThreeWords();
+        //2
+        checkSumSign();
+        //3
+        printCol();
+        //4
+        compareNumbers();
+        //5
+        System.out.println(checkSum());
+        //6
+        printNumberType();
+        //7
+        System.out.println(isNegative());
+        //8
+        printIsString("Hello people!!!",2);
+        //9
+        System.out.println(isLeapYear(2098));
+        //10
+        Array();
+        //11
+        Array100();
+        //12
+        transformArray();
+        //13
+        fillArray();
+        //14
+        System.out.println(Arrays.toString(returnArray(5, 1)));
 
     }
+
+    //1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    //2
+    public static void checkSumSign() {
+        int a = 7;
+        int b = 5;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    //3
+    public static void printCol() {
+        int value = 136;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value < 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    //4
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 24;
+        if (a >= b) {
+            System.out.println("a>=b");
+        }
+        if (a < b) {
+            System.out.println("a<b");
+        }
+    }
+
+    //5
+    public static boolean checkSum() {
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    //6
+    public static void printNumberType() {
+        int number = -25;
+        if (number >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    //7
+    public static boolean isNegative() {
+        int number = -5;
+        return number <= 0;
+    }
+
+    //8
+    public static void printIsString(String str, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(str);
+
+        }
+    }
+
+    //9
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+    }
+
+    //10
+    public static void Array() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
+        }
+    }
+    //11
+    public static void Array100() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //12
+    public static void transformArray() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] *= 2;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //13
+    public static void fillArray() {
+        int arrSize = 3;
+        int[][] arr = new int[arrSize][arrSize];
+        for (int i = 0; i < arrSize; i++) {
+            for (int j = 0; j < arrSize; j++) {
+                if (i == j || j == arrSize - i - 1) arr[i][j] = 1;
+                else arr[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+
+    //14
+    public static int[] returnArray(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
+
 }
+
+
