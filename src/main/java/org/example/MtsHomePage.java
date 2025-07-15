c9abe1a8dd88d4d10ae44f6109557e24cbddd1c8
SmaiL <vikaorlyanina97@gmail.com>
2025-07-15 12:42:53 +0300

    Тесты на "Онлайн пополнение без комиссии"

---
+++ src/main/java/org/example/MtsHomePage.java
@@ -0,0 +1,104 @@
+package org.example;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class MtsHomePage {
+    public WebDriver driver;
+
+
+    public MtsHomePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    //Локатор для заголовка
+    @FindBy(xpath = "//section/div/h2")
+    private WebElement titleBlock;
+
+    //Локатор для ссылки "Подробнее о сервисе"
+    @FindBy(xpath = "//section[@class='pay']//a[contains(text(),'Подробнее о сервисе')]")
+    private WebElement link;
+
+    //Локатор для поля телефон
+    @FindBy(xpath = "//input[@id='connection-phone']")
+    private WebElement phoneField;
+
+    //Локатор для поля сумма
+    @FindBy(xpath = "//input[@id='connection-sum']")
+    private WebElement sumField;
+
+    //Локатор для кнопки "Продолжить"
+    @FindBy(css = "#pay-connection > button")
+    private WebElement payBtn;
+
+    //Локатор для картинок
+    @FindBy(xpath = "//section[@class='pay']//img")
+    private List<WebElement> payImgages;
+
+    //локатора для кнопки "Принять" куки
+    @FindBy(xpath = "//div[2]/button[3]")
+    private WebElement cookieCancelBtn;
+
+
+    //Метод возвращает заголовок формы оплаты
+    public String getTitleBlock() {
+        return titleBlock.getText().replaceAll("\n|\r\n", " ");
+    }
+
+    //Метод возвращает url ссылки "Подробнее о сервисе"
+    public String getLinkUrl() throws NoSuchElementException {
+        return link.getAttribute("href");
+    }
+
+    //Метод возвращает код ответа ссылки
+    public int getRespCode(String urlLink) throws MalformedURLException, IOException {
+        URL url = new URL(urlLink);
+        HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
+        httpURLConnect.setConnectTimeout(10000);
+        httpURLConnect.connect();
+        return httpURLConnect.getResponseCode();
+    }
+
+    //Метод заполняет поле телефона формы пополнения счета
+    public void setPhoneField(String phoneNumber) throws NoSuchElementException {
+        phoneField.sendKeys(phoneNumber);
+    }
+
+    //Метод заполняет поле суммы формы пополнения счета
+    public void setSumField(String sum) throws NoSuchElementException {
+        sumField.sendKeys(sum);
+    }
+
+    //Метод нажимает на кнопку оплаты
+    public void clickPayBtn() throws NoSuchElementException {
+        payBtn.click();
+    }
+
+    //Возвращает отображается ли картинка именем src
+    public boolean isDisplayedImg(String scr) {
+        for (WebElement payImage : payImgages) {
+            if (payImage.getAttribute("src").contains(scr)) {
+                return payImage.isDisplayed();
+            }
+        }
+        return false;
+    }
+
+    // Метод прожимает куки
+    public void clickCookieCancelBtn() {
+        cookieCancelBtn.click();
+    }
+}
+
+
+
---
+++ src/test/java/org/example/MtsHomePageTest.java
@@ -0,0 +1,100 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import static javax.swing.UIManager.put;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MtsHomePageTest {
+    public static MtsHomePage mtsHomePage;
+    public static WebDriver driver;
+    public static final String PAGE_URL = "http://mts.by";
+    WebDriverWait wait;
+
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        mtsHomePage = new MtsHomePage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get(PAGE_URL);
+        mtsHomePage.clickCookieCancelBtn();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    @DisplayName("Заголовок формы")
+    public void title() {
+        String expectedTitle = "Онлайн пополнение без комиссии";
+        Assertions.assertTrue(mtsHomePage.getTitleBlock().contains(expectedTitle));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Картинки платежных систем")
+    @ValueSource(strings = {"visa.svg", "visa-verified.svg", "mastercard.svg", "mastercard-secure.svg", "belkart.svg"})
+    void payPics(String src) {
+        try {
+            assertTrue(mtsHomePage.isDisplayedImg(src), "Картинка " + src + " не отображается");
+            System.out.println("Картинка " + src + " отображается");
+        } catch (NoSuchElementException e) {
+            fail("Картинка " + src + " не найдена");
+        }
+    }
+
+    @Test
+    @DisplayName("Ссылка 'Подробнее о сервисе'")
+    void detailLink() {
+        String urlLink = null;
+        try {
+            urlLink = mtsHomePage.getLinkUrl();
+            int linkResponseCode = mtsHomePage.getRespCode(urlLink);
+            assertTrue(linkResponseCode < 400, "Ссылка " + urlLink + " битая (код: " + linkResponseCode + ")");
+            System.out.println("Ссылка " + urlLink + " рабочая (код: " + linkResponseCode + ")");
+        } catch (NoSuchElementException e) {
+            fail("Нет ссылки");
+        } catch (MalformedURLException e) {
+            fail("Не корректный url: " + urlLink);
+        } catch (IOException e) {
+            fail("Проблема с соединением");
+        }
+    }
+
+    @Test
+    @DisplayName("Работа кнопки 'Продолжить'")
+    void payForm() throws InterruptedException {
+        String name = "Окно оплаты";
+        try {
+            mtsHomePage.setPhoneField("297777777");
+            mtsHomePage.setSumField("10");
+            mtsHomePage.clickPayBtn();
+            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[contains(@class, 'bepaid-iframe')]")));
+            System.out.println(name + " открылось");
+        } catch (NoSuchElementException e) {
+            fail("Элемент не найден");
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
